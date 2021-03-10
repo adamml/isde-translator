0c84172839f3ce012f03811d3307d2c8835558c3
Adam Leadbetter <adamml@users.noreply.github.com>
2021-03-10 21:04:26 +0000

    Add files via upload

---
+++ ie/isde/ie/isde/__init__.py
@@ -0,0 +1,9 @@
+"""
+A package for working with metadata from the Irish Spatial Data Exchange
+"""
+
+from .ISDERDFNamespaces import ISDERDFNamespaces
+from .ISDEDatasetMetadata import ISDEDatasetMetadata
+
+__all__ = ['ISDERDFNamespaces',
+           'ISDEDatasetMetadata']
