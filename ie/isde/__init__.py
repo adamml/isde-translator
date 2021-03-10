b1b875f7c6991a2ae80849738597e674108c82b3
Adam Leadbetter <adamml@users.noreply.github.com>
2021-03-10 21:08:27 +0000

    Update __init__.py

--- ie/isde/__init__.py
+++ ie/isde/__init__.py
@@ -1,9 +1,1 @@
-"""
-A package for working with metadata from the Irish Spatial Data Exchange
-"""
 
-from .ISDERDFNamespaces import ISDERDFNamespaces
-from .ISDEDatasetMetadata import ISDEDatasetMetadata
-
-__all__ = ['ISDERDFNamespaces',
-           'ISDEDatasetMetadata']
